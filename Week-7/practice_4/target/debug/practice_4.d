c03f27462433d2b34e2fd05efd048b13b1dcfd4b
MEPAR453 <peace.ejembi@pau.edu.ng>
2025-11-12 20:25:22 +0100

    Invoking a function

---
+++ Week-7/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\COS\ 101\p.ejembiCOS101\Week-7\practice_4\target\debug\practice_4.exe: C:\Users\New\Documents\COS\ 101\p.ejembiCOS101\Week-7\practice_4\src\main.rs
---
+++ Week-7/practice_6/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\COS\ 101\p.ejembiCOS101\Week-7\practice_6\target\debug\practice_7.exe: C:\Users\New\Documents\COS\ 101\p.ejembiCOS101\Week-7\practice_6\src\main.rs
