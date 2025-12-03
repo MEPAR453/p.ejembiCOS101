51c0c893dd0d3d57fd6ffa442c01675dbfcaac7d
MEPAR453 <peace.ejembi@pau.edu.ng>
2025-12-03 16:37:18 +0100

    Assigning value of one variable to another variable

---
+++ Week-10/Practice_3/target/debug/Practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\COS\ 101\p.ejembiCOS101\Week-10\Practice_3\target\debug\Practice_3.exe: C:\Users\New\Documents\COS\ 101\p.ejembiCOS101\Week-10\Practice_3\src\main.rs
---
+++ Week-10/Practice_3/target/debug/deps/Practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\COS 101\p.ejembiCOS101\Week-10\Practice_3\target\debug\deps\Practice_3.d: src\main.rs
+
+C:\Users\New\Documents\COS 101\p.ejembiCOS101\Week-10\Practice_3\target\debug\deps\Practice_3.exe: src\main.rs
+
+src\main.rs:
